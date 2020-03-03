816088d1832ece78267090d54d8f94d93a4999e1
unknown <nabil.ayari@esprit.tn>
2020-03-04 00:30:11 +0100

    First Commit

---
+++ option-h.sh
@@ -0,0 +1,6 @@
+function option-h() {
+echo "[-version][-v][-help][-h][-d][-lp][-s][-lcpu][-save]"
+echo -e "\n${YELLOW} -lpy:    ${NC} Afficher l'Utilisation des Disques."
+echo -e "${YELLOW} -ldk:   ${NC} Afficher les Information Relatives au Disques."
+echo -e "${YELLOW} -v:    ${NC} Version.\n\n"
+}
